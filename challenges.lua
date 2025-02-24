6d8ff54bb38b4a7bcfb42c34ba2a79ce59a8a0aa
oxoboo <oxoboo@pm.me>
2025-02-23 23:10:13 -0600

    Add challenges

--- challenges.lua
+++ challenges.lua
@@ -14,3 +14,37 @@ SMODS.Challenge {
         cards = { {s='S',r='Q'}, {s='D',r='Q'}, {s='C',r='Q'}, {s='H',r='Q'} }
     }
 }
+
+SMODS.Challenge {
+    key = 'cerberus',
+    loc_txt = { name = 'Cerberus' },
+    unlocked = true,
+    jokers = {
+        { id = 'j_hyperdef_hydra', edition = 'negative', eternal = true },
+        { id = 'j_hyperdef_hydra', edition = 'negative', eternal = true },
+        { id = 'j_hyperdef_hydra', edition = 'negative', eternal = true }
+    },
+    deck = { type = 'Challenge Deck' }
+}
+
+SMODS.Challenge {
+    key = 'instrumentality',
+    loc_txt = { name = 'Instrumentality' },
+    unlocked = true,
+    rules = {
+        custom = {
+            { id = 'set_eternal_ante', value = 5 },
+            { id = 'set_joker_slots_ante', value = 5 }
+        }
+    },
+    restrictions = {
+        banned_cards = {
+            { id = 'v_petroglyph' },
+            { id = 'v_hieroglyph' }
+        }
+    },
+    jokers = {
+        { id = 'j_hyperdef_adam' }
+    },
+    deck = { type = 'Challenge Deck' }
+}
