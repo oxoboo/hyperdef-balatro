68a40a91124ba68beb934a7c022305726d6f2297
oxoboo <oxoboo@pm.me>
2025-02-23 19:48:45 -0600

    Remove test decks, leave disabled by default

--- decks.lua
+++ decks.lua
@@ -1,3 +1,5 @@
+local ENABLE_TEST_DECK = false
+
 function deck_add_joker(forced_key)
     local card = create_card('Joker', G.jokers, nil, nil, nil, nil, forced_key, 'deck')
     card:add_to_deck()
@@ -5,94 +7,30 @@ function deck_add_joker(forced_key)
     G.jokers:emplace(card)
 end
 
-SMODS.Back {
-    name = 'Test Deck 1',
-    key = 'test1',
-    pos = { x = 1, y = 0 },
-    loc_txt = {
-        name = 'Test Deck 1',
-        text = {
-            'Start run with',
-            '{C:attention}Creature{}'
+if ENABLE_TEST_DECK then
+    SMODS.Back {
+        name = 'Hyper Definition Test Deck',
+        key = 'hyperdef-test',
+        pos = { x = 0, y = 0 },
+        loc_txt = {
+            name = 'Hyper Definition Test Deck',
+            text = {
+                'This is a Test Deck',
+                'for {C:attention}Hyper Definition{}'
+            },
         },
-    },
-    unlocked = true,
-    discovered = true,
-    apply = function()
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                deck_add_joker('j_hyperdef_creature')
-                return true
-            end
-        }))
-    end
-}
-
-SMODS.Back {
-    name = 'Test Deck 2',
-    key = 'test2',
-    pos = { x = 1, y = 0 },
-    loc_txt = {
-        name = 'Test Deck 2',
-        text = {
-            'Start run with',
-            '{C:attention}Hydra the Fox{}'
-        },
-    },
-    unlocked = true,
-    discovered = true,
-    apply = function()
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                deck_add_joker('j_hyperdef_hydra')
-                return true
-            end
-        }))
-    end
-}
-
-SMODS.Back {
-    name = 'Test Deck 3',
-    key = 'test3',
-    pos = { x = 1, y = 0 },
-    loc_txt = {
-        name = 'Test Deck 3',
-        text = {
-            'Start run with',
-            '{C:attention}Horse Plinko{}',
-        },
-    },
-    unlocked = true,
-    discovered = true,
-    apply = function()
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                deck_add_joker('j_hyperdef_horse_plinko')
-                return true
-            end
-        }))
-    end
-}
-
-SMODS.Back {
-    name = 'Test Deck 4',
-    key = 'test4',
-    pos = { x = 1, y = 0 },
-    loc_txt = {
-        name = 'Test Deck 4',
-        text = {
-            'Start run with',
-            '{C:attention}Fish Satan{}'
-        },
-    },
-    unlocked = true,
-    discovered = true,
-    apply = function()
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                deck_add_joker('j_hyperdef_fish_satan')
-                return true
-            end
-        }))
-    end
-}
+        unlocked = true,
+        discovered = true,
+        apply = function()
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    -- ease_dollars(10)
+                    -- G.GAME.round_resets.hands = 5
+                    -- G.GAME.round_resets.discards = 4
+                    -- deck_add_joker('j_hyperdef_adam')
+                    return true
+                end
+            }))
+        end
+    }
+end
