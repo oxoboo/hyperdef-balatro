0023f37cf48e3e81730aca4edb4410576f36230a
oxoboo <126103011+oxoboo@users.noreply.github.com>
2025-03-31 20:18:25 -0500

    Rewrite boosters & add joker to booster

--- objects/boosters.lua
+++ objects/boosters.lua
@@ -1,3 +1,13 @@
+local hyper1_jokers = {
+    'j_hyperdef_wooden_kaiju',
+    'j_hyperdef_shopcorp',
+    'j_hyperdef_horse_plinko',
+    'j_hyperdef_magazine',
+    'j_hyperdef_fish_satan',
+    'j_hyperdef_manic',
+    'j_hyperdef_creature'
+}
+
 -- This booster is only for the Hyper Deck
 SMODS.Booster {
     key = 'hyper1',
@@ -5,17 +15,9 @@ SMODS.Booster {
     atlas = 'boosters',
     pos = { x = 0, y = 0 },
     config = {
-        extra = 7,
+        extra = #hyper1_jokers,
         choose = 1,
-        jokers = {
-            'j_hyperdef_shopcorp',
-            'j_hyperdef_horse_plinko',
-            'j_hyperdef_wooden_kaiju',
-            'j_hyperdef_magazine',
-            'j_hyperdef_fish_satan',
-            'j_hyperdef_creature',
-            'j_hyperdef_manic'
-        }
+        jokers = hyper1_jokers
     },
     discovered = true,
     cost = 2,
@@ -24,8 +26,19 @@ SMODS.Booster {
         return { vars = { self.config.choose, self.config.extra } }
     end,
     create_card = function(self, card, i)
-        return create_card('Joker', G.jokers, nil, nil, nil, nil, self.config.jokers[i], 'pack')
+        return create_card('Joker', G.jokers, nil, nil, nil, nil, self.config.jokers[i], 'hyper1')
     end
+}
+
+local hyper2_jokers = {
+    'j_hyperdef_stead_dog',
+    'j_hyperdef_buffer_joker',
+    'j_hyperdef_polycule_joker',
+    'j_hyperdef_alchemy',
+    'j_hyperdef_hydra',
+    'j_hyperdef_adam',
+    'j_hyperdef_oxoboo',
+    'j_hyperdef_stead'
 }
 
 -- This booster is only for the Hyper Deck
@@ -35,17 +48,9 @@ SMODS.Booster {
     atlas = 'boosters',
     pos = { x = 1, y = 0 },
     config = {
-        extra = 7,
+        extra = #hyper2_jokers,
         choose = 1,
-        jokers = {
-            'j_hyperdef_buffer_joker',
-            'j_hyperdef_stead_dog',
-            'j_hyperdef_alchemy',
-            'j_hyperdef_hydra',
-            'j_hyperdef_oxoboo',
-            'j_hyperdef_adam',
-            'j_hyperdef_stead'
-        }
+        jokers = hyper2_jokers
     },
     discovered = true,
     cost = 2,
@@ -54,6 +59,6 @@ SMODS.Booster {
         return { vars = { self.config.choose, self.config.extra } }
     end,
     create_card = function(self, card, i)
-        return create_card('Joker', G.jokers, nil, nil, nil, nil, self.config.jokers[i], 'pack')
+        return create_card('Joker', G.jokers, nil, nil, nil, nil, self.config.jokers[i], 'hyper2')
     end
 }
