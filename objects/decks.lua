999c69ac754fbe265512c69c1573e6ede5d3c27d
oxoboo <126103011+oxoboo@users.noreply.github.com>
2025-06-20 22:23:05 -0500

    Update test function

--- objects/decks.lua
+++ objects/decks.lua
@@ -8,11 +8,11 @@ local function add_joker(forced_key, card_area, key_append)
     return card
 end
 
--- Boosters can be added after G.shop_booster is initiated
-local function shop_add_booster(forced_key)
+-- Can be added to any card area for testing.
+local function add_booster(forced_key, card_area)
     local booster = Card(
-        G.shop_booster.T.x + G.shop_booster.T.w / 2,
-        G.shop_booster.T.y,
+        card_area.T.x + card_area.T.w / 2,
+        card_area.T.y,
         G.CARD_W * 1.27,
         G.CARD_H * 1.27,
         G.P_CARDS.empty,
@@ -20,7 +20,7 @@ local function shop_add_booster(forced_key)
         { bypass_discovery_center = true, bypass_discovery_ui = true }
     )
     booster:start_materialize()
-    G.shop_booster:emplace(booster)
+    card_area:emplace(booster)
     create_shop_card_ui(booster)
     return booster
 end
@@ -30,7 +30,6 @@ if ENABLE_TEST_DECK then
         name = 'Hyper Definition Test Deck',
         key = 'test',
         pos = { x = 0, y = 0 },
-        unlocked = true,
         discovered = true,
         apply = function()
             G.E_MANAGER:add_event(Event({
@@ -39,6 +38,7 @@ if ENABLE_TEST_DECK then
                     -- G.GAME.round_resets.hands = 5
                     -- G.GAME.round_resets.discards = 4
                     -- add_joker('j_hyperdef_adam', G.jokers, 'deck')
+                    -- add_booster('p_hyperdef_hyper_normal', G.consumeables)
                     return true
                 end
             }))
