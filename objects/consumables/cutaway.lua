a9015b3798f21929f19958c49a45b0d2a17fca5b
oxoboo <126103011+oxoboo@users.noreply.github.com>
2025-06-22 18:52:43 -0500

    Cutaway updates HUD when used

--- objects/consumables/cutaway.lua
+++ objects/consumables/cutaway.lua
@@ -1,3 +1,37 @@
+local function reset_blind_select_opt(opt, type)
+    local par = opt.parent
+    local bg_color = nil
+    if type == 'Boss' then
+        bg_color = mix_colours(G.C.BLACK, get_blind_main_colour(type), 0.8)
+    else
+        bg_color = lighten(G.C.BLACK, 0.07)
+    end
+    opt:remove()
+    opt = UIBox{
+        T = { par.T.x, 0, 0, 0 },
+        definition = {
+            n = G.UIT.ROOT, config = { align = "cm", colour = G.C.CLEAR },
+            nodes = { UIBox_dyn_container(
+                { create_UIBox_blind_choice(type) },
+                false,
+                get_blind_main_colour(type),
+                bg_color
+            )}
+        },
+        config = {
+            align = "bmi",
+            offset = {x = 0, y = G.ROOM.T.y + 9},
+            major = par,
+            xy_bond = 'Weak'
+        }
+    }
+    par.config.object = opt
+    par.config.object:recalculate()
+    opt.parent = par
+    opt.alignment.offset.y = 0
+    return opt
+end
+
 SMODS.Consumable({
     set = 'Spectral',
     key = 'cutaway',
@@ -29,6 +63,24 @@ SMODS.Consumable({
                 G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante or G.GAME.round_resets.ante
                 G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante + self.config.mod_antes
                 ease_dollars(-G.GAME.dollars + self.config.to_dollars)
+                -- change the blind amount during a round
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        if G.GAME.blind then
+                            G.GAME.blind.chips = get_blind_amount(G.GAME.round_resets.ante) * G.GAME.blind.mult * G.GAME.starting_params.ante_scaling
+                            G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
+                        end
+                        return true
+                    end
+                }))
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        G.blind_select_opts.small = reset_blind_select_opt(G.blind_select_opts.small, 'Small')
+                        G.blind_select_opts.big = reset_blind_select_opt(G.blind_select_opts.big, 'Big')
+                        G.blind_select_opts.boss = reset_blind_select_opt(G.blind_select_opts.boss, 'Boss')
+                        return true
+                    end
+                }))
                 return true
             end
         }))
