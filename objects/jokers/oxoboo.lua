149badd962338e2a55d22ecba672856a059de302
oxoboo <126103011+oxoboo@users.noreply.github.com>
2025-06-23 09:29:44 -0500

    Revert cost of Oxoboo

--- objects/jokers/oxoboo.lua
+++ objects/jokers/oxoboo.lua
@@ -4,7 +4,7 @@ SMODS.Joker {
     rarity = 3,
     -- atlas = 'jokers',
     -- pos = { x = 3, y = 1 },
-    cost = 9,
+    cost = 7,
     discovered = true,
     calculate = function(self, card, context)
         if context.end_of_round and not context.repetition and context.game_over == false and G.GAME.current_round.hands_played == 1 then
